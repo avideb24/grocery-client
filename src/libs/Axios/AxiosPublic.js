dee4d8d7478ff18b6a6b477b48976bd76e10d543
avideb24 <debavi24@gmail.com>
2024-12-21 13:10:49 +0600

    updated base url again

--- src/libs/Axios/AxiosPublic.js
+++ src/libs/Axios/AxiosPublic.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'https://grocery-server-1-zfom.onrender.com/'
+    baseURL: 'https://grocery-server-t3mo.onrender.com/'
 })
 
 const AxiosPublic = () => {
